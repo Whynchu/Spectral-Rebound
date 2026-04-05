99e6535032f9da19cbf4bc405b8ae191fc5b82ca
Whynchu <samuelboxold@gmail.com>
2026-04-05 15:16:52 -0400

    1.16.7 PATCH NOTES HUD FIX

--- src/data/version.js
+++ src/data/version.js
@@ -1,4 +1,4 @@
-const VERSION = { num: '1.16.6', label: 'PATCH NOTES FIX' };
+const VERSION = { num: '1.16.7', label: 'PATCH NOTES HUD FIX' };
 
 function formatVersionTag(version = VERSION) {
   return `// prototype v${version.num} - ${version.label}`;
