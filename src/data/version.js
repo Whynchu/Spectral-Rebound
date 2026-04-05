18c9a2bd5f37d40feac90f1e67e3134060d6a30c
Whynchu <samuelboxold@gmail.com>
2026-04-05 15:11:41 -0400

    1.16.6 PATCH NOTES FIX

--- src/data/version.js
+++ src/data/version.js
@@ -1,4 +1,4 @@
-const VERSION = { num: '1.16.5', label: 'PATCH NOTES' };
+const VERSION = { num: '1.16.6', label: 'PATCH NOTES FIX' };
 
 function formatVersionTag(version = VERSION) {
   return `// prototype v${version.num} - ${version.label}`;
